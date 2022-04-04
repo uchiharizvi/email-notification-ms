38b09fe4e30987a65bde4eeeb5120cfd78f0c166
Kavish Rizvi <rizvikavish205.com>
2022-04-04 23:03:31 +0530

    Initial Commit

---
+++ src/test/java/com/credentials/emailms/EmailMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credentials.emailms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
